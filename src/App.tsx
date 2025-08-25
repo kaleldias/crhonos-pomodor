6cfec5ea3d04a2317b4348f6c7781eb140b646c7
Kal-EL Dias <kaleldias@hashing3.com>
2025-08-25 20:27:19 -0300

    configs iniciais css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import './styles/theme.css';
+import './styles/global.css';
+import { Heading } from './components/Heading';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export function App() {
   return (
+    //sempre tem que ter um container pai encapsulando
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Alterei</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+      <Heading attr={2} attr2='string'>
+        Meus Ovos
+      </Heading>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
       </p>
     </>
   );
 }
-
-export default App;
